9e13bc598f50008bf278e020898d4b87969e91e6
ankitxcoding <ankitxcoding@gmail.com>
2024-02-27 21:20:43 +0530

    foodz-server for cors api fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 // For Restaurant API
-app.get('/api/restaurants', async (req, res) => {
+app.get('/dapi/restaurants/list/v5', async (req, res) => {
     const { lat, lng, page_type } = req.query;
     console.log(req.query);
 
@@ -40,7 +40,7 @@ app.get('/api/restaurants', async (req, res) => {
 
 
 // For Menu API
-app.get('/api/menu', async (req, res) => {
+app.get('/dapi/menu/pl', async (req, res) => {
     const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng, submitAction, restaurantId } = req.query;
     console.log(req.query);
 
